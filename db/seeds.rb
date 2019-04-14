65454d893da789ef3270c327a7cbb9136f348d60
nishisuke <nishisuke13+git@gmail.com>
2019-04-14 13:21:48 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+raise unless User.count.zero?
+
+%i[nishi suke tarou].each.with_index do |name, index|
+  User.create!(name: name, age: index * 10 + [*0..9].sample)
+end
+
+User.all.to_a.combination(2).with_index do |(user, mate), i|
+  next if i.modulo(3).zero?
+  Friend.create!(user_id: user.id, mate_user_id: mate.id)
+end
